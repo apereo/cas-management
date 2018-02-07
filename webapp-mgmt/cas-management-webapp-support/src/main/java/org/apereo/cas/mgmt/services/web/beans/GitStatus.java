4c73cfaad2973cd42f452317be5cd9e092879036
Travis Schmidt <tsschmidt@ucdavis.edu>
2018-02-07 14:55:27 -0800

    Lonboked GitStatus

--- webapp-mgmt/cas-management-webapp-support/src/main/java/org/apereo/cas/mgmt/services/web/beans/GitStatus.java
+++ webapp-mgmt/cas-management-webapp-support/src/main/java/org/apereo/cas/mgmt/services/web/beans/GitStatus.java
@@ -1,5 +1,9 @@
 package org.apereo.cas.mgmt.services.web.beans;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 import java.util.Set;
 
 /**
@@ -8,59 +12,13 @@ import java.util.Set;
  * @author Travis Schmidt
  * @since 5.2.2
  */
-public class GitStatus {
+@Data
+@NoArgsConstructor
+public class GitStatus implements Serializable {
     private boolean hasChanges;
     private boolean unpublished;
     private boolean pendingSubmits;
     private Set<String> added;
     private Set<String> modified;
     private Set<String> deleted;
-
-    public boolean isHasChanges() {
-        return hasChanges;
-    }
-
-    public void setHasChanges(final boolean hasChanges) {
-        this.hasChanges = hasChanges;
-    }
-
-    public Set<String> getAdded() {
-        return added;
-    }
-
-    public void setAdded(final Set<String> added) {
-        this.added = added;
-    }
-
-    public Set<String> getModified() {
-        return modified;
-    }
-
-    public void setModified(final Set<String> modified) {
-        this.modified = modified;
-    }
-
-    public Set<String> getDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(final Set<String> deleted) {
-        this.deleted = deleted;
-    }
-
-    public boolean isUnpublished() {
-        return unpublished;
-    }
-
-    public void setUnpublished(final boolean unpublished) {
-        this.unpublished = unpublished;
-    }
-
-    public boolean isPendingSubmits() {
-        return pendingSubmits;
-    }
-
-    public void setPendingSubmits(final boolean pendingSubmits) {
-        this.pendingSubmits = pendingSubmits;
-    }
 }

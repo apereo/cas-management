2264f3611c4ef2a44e0020f49592e0f8f1add8f9
Nisith Dash <ndash@unicon.net>
2021-04-29 15:45:29 -0700

    Closes #IAMSE-1213 (https://jira.unicon.net/jira/projects/IAMSE/issues/IAMSE-1213)
    Changing the service name during service edits creates duplicate service config JSON files with the same service ID. The check for rename() fails due to a bug in RegisteredServiceResourceNamingStrategy.
    
    Return the default/correct implementation for RegisteredServiceResourceNamingStrategy Bean in the WebAppConfiguration.

--- webapp/cas-mgmt-webapp-config/src/main/java/org/apereo/cas/mgmt/config/CasManagementWebAppConfiguration.java
+++ webapp/cas-mgmt-webapp-config/src/main/java/org/apereo/cas/mgmt/config/CasManagementWebAppConfiguration.java
@@ -6,7 +6,7 @@ import org.apereo.cas.mgmt.controller.ViewController;
 import org.apereo.cas.mgmt.web.DefaultCasManagementEventListener;
 import org.apereo.cas.oidc.claims.BaseOidcScopeAttributeReleasePolicy;
 import org.apereo.cas.oidc.claims.OidcCustomScopeAttributeReleasePolicy;
-import org.apereo.cas.services.RegisteredService;
+import org.apereo.cas.services.resource.DefaultRegisteredServiceResourceNamingStrategy;
 import org.apereo.cas.services.resource.RegisteredServiceResourceNamingStrategy;
 import org.apereo.cas.util.CollectionUtils;
 
@@ -97,7 +97,7 @@ public class CasManagementWebAppConfiguration implements WebMvcConfigurer {
             protected Locale determineDefaultLocale(final HttpServletRequest request) {
                 val locale = request.getLocale();
                 if (StringUtils.isEmpty(managementProperties.getDefaultLocale())
-                    || !locale.getLanguage().equals(managementProperties.getDefaultLocale())) {
+                        || !locale.getLanguage().equals(managementProperties.getDefaultLocale())) {
                     return locale;
                 }
                 return new Locale(managementProperties.getDefaultLocale());
@@ -123,9 +123,9 @@ public class CasManagementWebAppConfiguration implements WebMvcConfigurer {
     public Collection<BaseOidcScopeAttributeReleasePolicy> userDefinedScopeBasedAttributeReleasePolicies() {
         val oidc = casProperties.getAuthn().getOidc();
         return oidc.getUserDefinedScopes().entrySet()
-            .stream()
-            .map(k -> new OidcCustomScopeAttributeReleasePolicy(k.getKey(), CollectionUtils.wrapList(k.getValue().split(","))))
-            .collect(Collectors.toSet());
+                .stream()
+                .map(k -> new OidcCustomScopeAttributeReleasePolicy(k.getKey(), CollectionUtils.wrapList(k.getValue().split(","))))
+                .collect(Collectors.toSet());
     }
 
     @RefreshScope
@@ -137,11 +137,7 @@ public class CasManagementWebAppConfiguration implements WebMvcConfigurer {
     @Bean(name = "namingStrategy")
     @RefreshScope
     public RegisteredServiceResourceNamingStrategy registeredServiceResourceNamingStrategy() {
-        return new RegisteredServiceResourceNamingStrategy() {
-            @Override
-            public String build(final RegisteredService service, final String extenstion) {
-                return service.getName() + "-" + service.getId() + ".json";
-            }
+        return new DefaultRegisteredServiceResourceNamingStrategy() {
         };
     }
 
@@ -163,7 +159,7 @@ public class CasManagementWebAppConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-            .addResourceLocations("classpath:/dist/", "classpath:/static/");
+                .addResourceLocations("classpath:/dist/", "classpath:/static/");
     }
 
     @Bean

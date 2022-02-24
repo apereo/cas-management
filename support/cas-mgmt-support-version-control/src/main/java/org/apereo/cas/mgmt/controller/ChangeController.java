519edc5c8c7973aeacd3606ba39274781ab0bdfa
Ryan Mathis <rmathis@unicon.net>
2022-02-24 13:17:16 -0700

    Fixed checkstyle error

--- support/cas-mgmt-support-version-control/src/main/java/org/apereo/cas/mgmt/controller/ChangeController.java
+++ support/cas-mgmt-support-version-control/src/main/java/org/apereo/cas/mgmt/controller/ChangeController.java
@@ -22,8 +22,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 

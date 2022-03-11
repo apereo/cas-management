7443b1363a385b097c5d69ecde2a2680ec32df01
Ryan Mathis <rmathis@unicon.net>
2022-03-11 13:13:13 -0700

    Fixed metrics data for dashboard

--- webapp/cas-mgmt-webapp-workspace/projects/dashboard/src/app/domain/dashboard.model.ts
+++ webapp/cas-mgmt-webapp-workspace/projects/dashboard/src/app/domain/dashboard.model.ts
@@ -1,31 +1,98 @@
+export type ServerStatus = 'UP' | 'WARN' | 'DOWN';
+
+
 /**
  * Server model.
  */
-export class Server {
+export interface Server {
   name: string;
-  system: SystemHealth;
+  health: SystemHealth;
 }
 
 /**
  * SystemHealth model.
  */
-export class SystemHealth {
-  status: string;
-  details: Details | null;
+export interface SystemHealth {
+  status: ServerStatus;
+  components: Components | null;
 }
 
 /**
  * Details model.
  */
-export class Details {
-  systemUsage: number;
-  systemLoad: number;
-  processUsage: number;
-  jvmUsed: number;
-  jvmCommitted: number;
-  heapUsed: number;
-  heapCommitted: number;
-  uptime: number;
-  requests: number;
-  maxRequest: number;
+export interface Components {
+  memory: MemoryComponent;
+  ping: {
+    status: ServerStatus;
+  }
+  refreshScope: {
+    status: ServerStatus;
+  }
+  session: SessionComponent;
+  system: SystemComponent;
 }
+
+export interface MemoryComponent {
+  status: ServerStatus;
+  details: {
+    freeMemory: number;
+    totalMemory: number;
+  }
+}
+
+export interface SessionComponent {
+  status: ServerStatus;
+  details: {
+    name: string;
+    sessionCount: 0;
+    ticketCount: 0;
+    message: string;
+  }
+}
+
+export interface SystemComponent {
+  status: ServerStatus;
+  details: {
+    systemUsage: number;
+    systemLoad: number;
+    processUsage: number;
+    jvmUsed: number;
+    jvmCommitted: number;
+    heapUsed: number;
+    heapCommitted: number;
+    uptime: number;
+    requests: number;
+    maxRequest: number;
+  };
+}
+
+/*
+
+[
+  {
+    name: "CAS Local",
+    health: {
+      status: "UP",
+      components: {
+        memory: {
+          status: "UP",
+          details: { freeMemory: 1817246480, totalMemory: 2147483648 },
+        },
+        ping: { status: "UP" },
+        refreshScope: { status: "UP" },
+        session: {
+          status: "UP",
+          details: {
+            name: "TicketRegistryHealthIndicator",
+            sessionCount: 0,
+            ticketCount: 0,
+            message: "OK",
+          },
+        },
+      },
+    },
+  },
+  { name: "CAS on Heroku", health: { status: "UP" } },
+];
+
+*/
